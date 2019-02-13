c1e49c811d320104cc736d4f0a0a68242807c959
Sergei Golubchik <serg@mariadb.org>
2019-02-13 20:35:11 +0100

    cleanup: Account_options
    move account options from LEX to Account_options structure
    namely, mqh and ssl_*
    
    Also, use LEX_CSTRING for ssl_*/x509_* strings and move
    setting of ACL_USER::account_locked where it belongs

--- sql/sql_lex.h
+++ sql/sql_lex.h
@@ -2936,7 +2936,7 @@ enum account_lock_type
   ACCOUNTLOCK_UNLOCKED
 };
 
-struct Account_options
+struct Account_options: public USER_RESOURCES
 {
   Account_options()
     : account_locked(ACCOUNTLOCK_UNSPECIFIED)
@@ -2944,9 +2944,12 @@ struct Account_options
 
   void reset()
   {
-    account_locked= ACCOUNTLOCK_UNSPECIFIED;
+    bzero(this, sizeof(*this));
+    ssl_type= SSL_TYPE_NOT_SPECIFIED;
   }
 
+  enum SSL_type ssl_type;                       // defined in violite.h
+  LEX_CSTRING x509_subject, x509_issuer, ssl_cipher;
   account_lock_type account_locked;
 };
 
@@ -3009,7 +3012,6 @@ public:
   const char *help_arg;
   const char *backup_dir;                       /* For RESTORE/BACKUP */
   const char* to_log;                           /* For PURGE MASTER LOGS TO */
-  const char* x509_subject,*x509_issuer,*ssl_cipher;
   String *wild; /* Wildcard in SHOW {something} LIKE 'wild'*/ 
   sql_exchange *exchange;
   select_result *result;
@@ -3115,7 +3117,6 @@ public:
   LEX_MASTER_INFO mi;                              // used by CHANGE MASTER
   LEX_SERVER_OPTIONS server_options;
   LEX_CSTRING relay_log_connection_name;
-  USER_RESOURCES mqh;
   LEX_RESET_SLAVE reset_slave_info;
   ulonglong type;
   ulong next_binlog_file_number;
@@ -3153,7 +3154,6 @@ public:
   */
   bool parse_vcol_expr;
 
-  enum SSL_type ssl_type;                       // defined in violite.h
   enum enum_duplicates duplicates;
   enum enum_tx_isolation tx_isolation;
   enum enum_ha_read_modes ha_read_mode;
